dd90a12cd1d1a564c20f09180ac7b9f9528fb61e
Felix Fennell <felnne@bas.ac.uk>
2021-02-09 23:03:30 +0000

    Adding basic app logging

--- app.py
+++ app.py
@@ -1,6 +1,13 @@
-def run():
+import logging
+import sys
+from pathlib import Path
+
+
     pass
 
+
+def run() -> None:
+    logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
 
 if __name__ == "__main__":
     run()
