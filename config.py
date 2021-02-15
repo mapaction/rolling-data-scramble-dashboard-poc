2afbcb9a7ad28f12a26fa9d98dca9ca4ba7257f1
Felix Fennell <felnne@bas.ac.uk>
2021-02-15 12:52:31 +0000

    Refactoring Google Sheets exporter
    Renamed method name to match `export_foo` convention.
    
    Reading export data directly, rather than loading from JSON file.
    
    Refactoring how dataframes are constructed from export data. I’ve changed this to construct from a pre-formed dict which may be wrong but it seemed the most direct way to me.
    
    In any case, they should now be easier to construct as things like labels for result types and getting the country name for an operation are included in the export data.
    
    Renamed config options and added to TypedDict.
    
    The core uploading logic is largely unchanged.

--- config.py
+++ config.py
@@ -8,6 +8,11 @@ class Config(TypedDict):
     rds_operations_cmf_paths: List[Path]
     all_products_product_id: str
     export_path: Path
+    google_service_credential_path: Path
+    google_service_credential_scopes: List[str]
+    google_sheets_key: str
+    google_sheets_summary_sheet_name: str
+    google_sheets_detail_sheet_name: str
 
 
 config: Config = dict()
@@ -28,9 +33,8 @@ config["all_products_product_id"] = "MA9999"
 
 config["export_path"] = Path("export.json")
 
-config["service_credential"] = Path("google-application-credentials.json")
-config["spreadsheet_scope"] = ["https://spreadsheets.google.com/feeds"]
-config["spreadsheet_key"] = "1MSXc-1mffyv_EtiXWvpu-cDc92UAutRkXVFV4ICILx8"
-config["sheet_name_summary"] = "Summary"
-config["sheet_name_complete"] = "All layers"
-
+config["google_service_credential_path"] = Path("google-application-credentials.json")
+config["google_service_credential_scopes"] = ["https://spreadsheets.google.com/feeds"]
+config["google_sheets_key"] = "1MSXc-1mffyv_EtiXWvpu-cDc92UAutRkXVFV4ICILx8"
+config["google_sheets_summary_sheet_name"] = "Summary"
+config["google_sheets_detail_sheet_name"] = "All layers"
