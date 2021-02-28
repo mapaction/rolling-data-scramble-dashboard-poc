da5681eaeb52a196a3336147d4c3bf47c139d504
Felix Fennell <felnne@bas.ac.uk>
2021-02-28 15:16:25 +0000

    Fix

--- src/mapaction_rds_dashboard/__init__.py
+++ src/mapaction_rds_dashboard/__init__.py
@@ -1,6 +1,6 @@
 """MapAction Rolling Data Scramble (RDS) Dashboard."""
 try:
-    from importlib.metadata import version, PackageNotFoundError  # type: ignore
+    from importlib.metadata import version, PackageNotFoundError
 except ImportError:  # pragma: no cover
     from importlib_metadata import version, PackageNotFoundError  # type: ignore
 
